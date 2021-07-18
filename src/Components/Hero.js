af8f10c1aeb6f547cd629a9e66c2638bf119e5df
Milan <milan.sachani.ms@gmail.com>
2021-07-18 10:50:49 +0200

    IMG ADDED

--- src/Components/Hero.js
+++ src/Components/Hero.js
@@ -1,14 +1,18 @@
 import React from "react";
+import "./Hero.css";
 
 const Hero = ({ handleLogout }) => {
+  var img = document.createElement("img");
+  img.src =
+    "https://media.geeksforgeeks.org/wp-content/uploads/20190529122828/bs21.png";
   return (
     <section className="hero">
       <nav>
-        <h2> welcome to Milan's Gym</h2>
+        <h2>M-GYM</h2>
+        <h2>We are Gald to see you!</h2>
         <button onClick={handleLogout}> LogOut </button>
       </nav>
     </section>
-    
   );
 };
 
